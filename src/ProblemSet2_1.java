a064dff4f5fa17ddba055e337e3f82b4b7b22302
Aluno <Aluno@LAB04-PC05>
2024-06-04 21:25:14 -0300

    Art HappyFace

---
+++ src/ProblemSet2_1.java
@@ -0,0 +1,14 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        System.out.println("   XXXXXX");
+        System.out.println(" X        X");
+        System.out.println("X  @    @  X");
+        System.out.println("X          X");
+        System.out.println("X  X    X  X");
+        System.out.println("X   XXXX   X");
+        System.out.println("  X      X");
+        System.out.println("    XXXXX");
+        
+        
+    }   
+}
